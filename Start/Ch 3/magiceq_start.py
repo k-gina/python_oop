38e597259d2e6f8f0a74a53802b79aa784f1ef06
gina-kim <gina.kim@justeattakeaway.com>
2025-11-06 11:17:45 +0000

    chapter 3

--- Start/Ch 3/magiceq_start.py
+++ Start/Ch 3/magiceq_start.py
@@ -11,9 +11,24 @@ class Book:
 
     # TODO: the __eq__ method checks for equality between two objects
 
+    def __eq__(self, value):
+        if not isinstance(value, Book):
+            raise ValueError("Cannot compare Book with non-Book type")
+        return (self.title == value.title and
+                self.author == value.author and
+                self.price == value.price)
+
     # TODO: the __ge__ establishes >= relationship with another obj
+    def __ge__(self, value):
+        if not isinstance(value, Book):
+            raise ValueError("Cannot compare Book with non-Book type")
+        return self.price >= value.price
 
     # TODO: the __lt__ establishes < relationship with another obj
+    def __lt__(self, value):
+        if not isinstance(value, Book):
+            raise ValueError("Cannot compare Book with non-Book type")
+        return self.price < value.price
 
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
@@ -22,9 +37,15 @@ b3 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b4 = Book("To Kill a Mockingbird", "Harper Lee", 24.95)
 
 # TODO: Check for equality
-
+print(b1==b3) # returns false if __eq__ is not defined, because python doesn't compare objects by value by default
+print(b1==b2)
+print(b1==42)
 
 # TODO: Check for greater and lesser value
-
+print(b2>=b1)
+print(b2<b1)
 
 # TODO: Now we can sort them too
+books = [b1,b3,b2,b4]
+books.sort() # built in sort function sorts based on __lt__ method 
+print([book.title for book in books])
